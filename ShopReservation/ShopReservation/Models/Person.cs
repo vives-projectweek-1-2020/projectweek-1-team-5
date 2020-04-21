99bd95eca5a4c427e49b6b9b4a8c37e31f4467c8
lambo112 <lammertpuntbakker@gmail.com>
2020-04-21 21:14:31 +0200

    Added home page into project & some test

---
+++ ShopReservation/ShopReservation/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopReservation.Models
+{
+    public class Person
+    {
+        
+            public int PersonId { get; set; }
+
+            [Display(Name = "Name")]
+            public string PersonName { get; set; }
+            public string PassWord { get; set; }
+    }
+}
