80e0e465d9fb6181f9ab3abf4a76203d6e165514
lambo112 <lammertpuntbakker@gmail.com>
2020-04-21 10:59:45 +0200

    aanpassing project

---
+++ ShopReservation/ShopReservation/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopReservation.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopReservation.Controllers
+{
+    public class ReservationController : Controller
+    {   
+
+        //public List<ShopModel> ;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
