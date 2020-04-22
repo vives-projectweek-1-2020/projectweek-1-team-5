52e8ebabc6dba6e33ae7abce1c4b7cd4c184cc19
lambo112 <lammertpuntbakker@gmail.com>
2020-04-22 08:51:24 +0200

    Added project

---
+++ ShopReservation/ShopReservation/Models/ShopModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopReservation.Models
+{
+    public class ShopModel
+    {
+        public string ShopName { get; set; }
+        public int Reservations { get; set; }
+        public int MaxReservations { get; set; }
+    }
+}
