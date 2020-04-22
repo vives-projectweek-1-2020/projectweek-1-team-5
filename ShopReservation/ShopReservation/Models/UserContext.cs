7dbd75584a903bea02222706a218484479ab0697
lambo112 <lammertpuntbakker@gmail.com>
2020-04-22 09:48:23 +0200

    added db & user model & usercontext

---
+++ ShopReservation/ShopReservation/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopReservation.Models;
+
+namespace ShopReservation.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Movie { get; set; }
+    }
+}
