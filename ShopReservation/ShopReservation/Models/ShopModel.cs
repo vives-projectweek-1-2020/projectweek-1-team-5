5e81d944c47b35231bdbec9daec454ee3cc705f3
lambo112 <lammertpuntbakker@gmail.com>
2020-04-24 09:07:52 +0200

    Added reserve option + my jpg

--- ShopReservation/ShopReservation/Models/ShopModel.cs
+++ ShopReservation/ShopReservation/Models/ShopModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,12 @@ namespace ShopReservation.Models
     public class ShopModel
     {
         public int Id { get; set; }
+        [DisplayName("Shop name")]
         public string Name { get; set; }
         public string Time { get; set; }
+        [DisplayName("Reserved Spots")]
         public int Reserved { get; set; }
+        [DisplayName("Max amount of spots")]
         public int Spots { get; set; }
     }
 }
